4f1c54d134457068811b19d1cc5e072b8f056362
shrutikherud <shruti.kherud@gmail.com>
2025-01-31 13:23:32 +0530

    Initial commit

---
+++ ep-01/index.js
@@ -0,0 +1,19 @@
+//template literals
+
+const age = 22;
+const fullName = "Shruti Kherud";
+
+//output : Hello Ms Shruti Kherud your age is 22.
+
+console.log("Hello Ms "+fullName+" your age is "+age);
+
+console.log(`Hello Ms ${fullName} your age is: ${age}`);//can be used multiline
+
+
+//string concetenatw with number = string
+//Print sum of two numbers
+const a = 10;
+const b = 20;
+
+console.log(`Sum of ${a} and ${b} is: ${a+b}`);
+
